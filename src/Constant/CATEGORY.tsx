d30951529126b1106cad0ab258c832c7f9c28a13
Lawrence Liu <lsk21007@gmail.com>
2023-01-30 20:21:41 -0800

    wrap common component

---
+++ src/Constant/CATEGORY.tsx
@@ -0,0 +1,15 @@
+const CATEGORY: string[] = [
+  "burger",
+  "pizza",
+  "salad",
+  "rice",
+  "noodle",
+  "pasta",
+  "dumpling",
+  "potato",
+  "beef",
+  "lamb",
+  "pork",
+  "chicken",
+];
+export default CATEGORY;
---
+++ src/Constant/ICONS.tsx
@@ -0,0 +1,28 @@
+import burger from "../assets/svg/burger.svg";
+import pizza from "../assets/svg/pizza.svg";
+import salad from "../assets/svg/salad.svg";
+import rice from "../assets/svg/rice.svg";
+import noodle from "../assets/svg/noodle.svg";
+import pasta from "../assets/svg/pasta.svg";
+import dumpling from "../assets/svg/dumpling.svg";
+import potato from "../assets/svg/potato.svg";
+import beef from "../assets/svg/beef.svg";
+import lamb from "../assets/svg/lamb.svg";
+import pork from "../assets/svg/pork.svg";
+import chicken from "../assets/svg/chicken.svg";
+
+const ICONS: string[] = [
+  burger,
+  pizza,
+  salad,
+  rice,
+  noodle,
+  pasta,
+  dumpling,
+  potato,
+  beef,
+  lamb,
+  pork,
+  chicken,
+];
+export default ICONS;
